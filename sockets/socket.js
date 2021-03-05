696e195e9541b0b2a0fa7b8764f3341f419de3bf
Waldir Ozuna <wozuna@hotmail.com>
2021-03-04 22:19:28 -0600

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,12 @@
+const {io} = require('../index')
+//Mensajes de Socket
+io.on('connection', client=>{
+    console.log('Cliente conectado');
+    client.on('disconnect', ()=>{
+        console.log('Cliente desconectado');
+    });
+    client.on('mensaje', (payload)=>{
+        console.log('Mensaje', payload);  
+        io.emit('mensaje', {admin: 'nuevo mensaje'});
+    });
+});
